ee12532d891a2ee7e17d61bfdfed9a08c7bca184
e <569835014@qq.com>
2018-01-02 16:25:24 +0800

    vue 公共方法 和自定义校验指令

---
+++ ccav.js
@@ -0,0 +1,20 @@
+
+//
+//
+// import {req, withParams} from 'vuelidate/lib/validators/common'
+//
+// // export default (obj) =>
+// //   withParams({type: 'between', min, max}, value =>
+// //     !req(value) || (!/\s/.test(value) && +min <= +value && +max >= +value))
+//
+//
+// // export default (obj) =>
+// //   withParams({type: 'between', min, max}, value =>
+// //     !req(value) || (!/\s/.test(value) && +min <= +value && +max >= +value))
+//
+// var _common = require('vuelidate/lib/validators/common');
+//
+// var emailRegex = /^1[34578]\d{9}$/;
+//
+//
+// export default  (0, _common.regex)('phone', emailRegex);
---
+++ observer.js
@@ -0,0 +1,55 @@
+//
+const Observer = {
+  // 通过on接口监听事件eventName
+  // 如果事件eventName被触发，则执行callback回调函数
+  handles:{},
+  on: function (eventName, callback) {
+    //你的代码
+    if(!this.handles){
+      //this.handles={};
+      Object.defineProperty(this, "handles", {
+        value: {},
+        enumerable: false,
+        configurable: true,
+        writable: true
+      })
+    }
+
+    if(!this.handles[eventName]){
+      this.handles[eventName]=[];
+    }
+    if(this.handles[eventName].indexOf(callback)<0){
+      this.handles[eventName].push(callback);
+    }
+  },
+  // 触发事件 eventName
+  emit: function (eventName) {
+    //你的代码
+    if(this.handles[eventName]){
+      for(let i=0;i<this.handles[eventName].length;i++){
+        let arr=Array.prototype.slice.call(arguments).splice(1);
+        this.handles[eventName][i].apply(this,arr);
+      }
+    }
+    // this.handles[eventName]=[];
+  },
+  off:function (eventName,callback) {
+    if(!callback){
+      this.handles[eventName]=[];
+      return
+    }
+    let onFn=this.handles[eventName];
+    if(onFn){
+      let index=onFn.indexOf(callback);
+      if(index>-1){
+        onFn.splice(index,1)
+      }
+    }
+  }
+};
+const Ob={}
+Ob.install = function (Vue) {
+  // 4. 添加实例方法
+  Vue.prototype.$observer = Observer
+};
+export default Ob
